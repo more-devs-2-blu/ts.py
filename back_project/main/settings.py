2e5e20d6a3b895063c48130c9d38e266f4d3abd4
Marcos Silveira <marcosilveira.lg@gmail.com>
2023-03-15 23:52:22 -0300

    envio do back-end

--- back_project/main/settings.py
+++ back_project/main/settings.py
@@ -41,6 +41,29 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT'
+]
+
+
+ROOT_URLCONF = 'main.urls'
+
+CORS_ALLOWED_ORIGINS = [""]
+
+
 ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
